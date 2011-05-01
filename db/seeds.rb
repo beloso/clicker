bdd53de62e1f15bd672bcfff6b3d0f16db40c723
Tiago Veloso <ti.veloso@gmail.com>
2011-05-01 21:32:16 +0100

    Some db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.create([
+  {
+    :name => "ForcadoRules",
+    :url  => "http://www.darkthrone.com/recruiter/outside/B7OE4OA0OD8OD5OF1",
+    :clicks_given => 37,
+    :clicks_received => 47
+  },
+  {
+    :name => "Angele",
+    :url  => "http://www.darkthrone.com/recruiter/outside/B7OD8OE6OC2OF9OD5",
+    :clicks_given => 100,
+    :clicks_received => 10
+  },
+  {
+    :name => "jonash_",
+    :url  => "http://www.darkthrone.com/recruiter/outside/D5OD5OD5OB7OB7OF1OA0",
+    :clicks_given => 49,
+    :clicks_received => 21
+  }
+  ])

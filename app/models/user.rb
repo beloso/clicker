ae2b65d991cf96d0be17ebea4bdf0e4fcb99af50
Tiago Veloso <ti.veloso@gmail.com>
2011-05-01 21:26:12 +0100

    Minor cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,18 @@ class User < ActiveRecord::Base
   def default_values
     self.clicks_given    = 25 unless self.clicks_given
     self.clicks_received = 0  unless self.clicks_received
-    # => self.admin           = false unless self.admin
   end
   
   def credits
     credits = self.clicks_given - self.clicks_received
   end
   
-  validates :name,        :presence   => true
+  validates                 :name, :presence => true
   validates_uniqueness_of   :name
   
-  validates :url,         :presence   => true
-  validates_format_of     :url, :with => /\A(http\:\/\/)?(gold|www)?(\.)?darkthrone\.com\/recruiter\/outside\/[A-Z0-9]+\Z/
+  validates                 :url,  :presence => true
+  validates_format_of       :url,  :with     => /\A(http\:\/\/)?(gold|www)?(\.)?darkthrone\.com\/recruiter\/outside\/[A-Z0-9]+\Z/
   validates_uniqueness_of   :url
-  
   
   validates_numericality_of :clicks_given,    :only_integer => true, :greater_than_or_equal_to => 0
   validates_numericality_of :clicks_received, :only_integer => true, :greater_than_or_equal_to => 0

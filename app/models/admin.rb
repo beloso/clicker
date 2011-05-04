26752d12c4191e568717068f088022f09ca02731
Tiago Veloso <ti.veloso@gmail.com>
2011-05-04 10:53:11 +0100

    Fixing the admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   def email_head
     self.email.split('@')[0]

87fed1d933016332096aacfb3b93835f5c17a117
Tiago Veloso <ti.veloso@gmail.com>
2011-05-09 22:05:36 +0100

    Minor tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   MINIMUM_CREDITS = -5
-  
+
   before_filter :authenticate_admin!, :except => [:index, :show, :new, :click, :create]
-    
+
   # GET /users
   # GET /users.xml
   def index
@@ -11,11 +11,11 @@ class UsersController < ApplicationController
     frozen_users(@users)
     legend_users(@users)
     sorted_users(@users)
-    
+
     session.delete(:click_order)
     session.delete(:current_user)
     @selected_user = nil
-    
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @users }
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   # GET /users/new.xml
   def new
     @user = User.new
-    
+
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @user }
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   # GET /users/new.xml
   def new_batch
     @users
-    
+
     respond_to do |format|
       format.html # new_batch.html.erb
       format.xml  { render :xml => @user }
@@ -101,7 +101,7 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   # PUT /users/
   # PUT /users/
   def reset_counters
@@ -118,88 +118,80 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-  
+
     respond_to do |format|
       format.html { redirect_to users_path, :notice => @user.name + ' was successfully deleted.' }
       format.xml  { head :ok }
     end
   end
-  
+
   # DELETE /users/
   # DELETE /users/
   def destroy_frozen
     deleted = User.delete_frozen
-    
+
     respond_to do |format|
       format.html { redirect_to :back, :notice => "#{helpers.pluralize(deleted, 'inactive user was', 'inactive users were')} successfully deleted." }
       format.xml  { head :ok }
     end
   end
-  
+
   # GET /users/1/click
   # GET /users/1/click.xml
   def click
     @user = User.find(params[:id])
-    
+
     session[:click_order] ||= load_click_order
     session[:current_user] ||= load_current_user
-        
+    @clicked_user ||= load_clicked_user
+
     @next_user = next_in_array @user.id, session[:click_order]
-    
+
     if !session[:current_user].blank? && @selected_user.nil?
       @selected_user ||= User.find(session[:current_user])
     else
       flash[:error] = 'You must select a user.'
-      redirect_to :action => 'index'
-      return
+      redirect_to :action => 'index' and return
     end
-    
-    if !params[:clicked_user].blank?
-        @clicked_user ||= User.find(params[:clicked_user])
-    end
-    
-    if @clicked_user != @selected_user
-      process_click
-    else
-      flash[:notice] = 'Successfully clicked ' + @clicked_user.name + '.'
-    end
+
+    process_click
     
     if params[:commit] == 'End Clicking'
       redirect_to :action => 'index' and return
     end
-    
+
     respond_to do |format|
       format.html # click.html.erb
       format.xml  { render :xml => @user }
     end
   end
-  
+
   private
-  
+
   def all_users
     @users ||= User.ordered_by_credits.to_a
   end
-  
+
   def clickable_users(users)
     @clickables ||= users.select { |u| u.credits >= MINIMUM_CREDITS && !u.legend }.to_a
   end
-  
+
   def frozen_users(users)
     @frozen ||= users.select { |u| u.credits < MINIMUM_CREDITS && !u.legend }.to_a
   end
-  
+
   def legend_users(users)
     @legends ||= @users.select {|u| u.legend }.to_a
   end
-  
+
   def sorted_users(users)
     @sorted_by_name ||= users.sort { |a,b| a.name.downcase <=> b.name.downcase }
   end
-  
+
   def next_in_array(elem,array)
     array[ array.index(elem) + 1 ]
   end
-  
+
   def load_click_order
     if session[:click_order].blank?
       all_users
@@ -207,7 +199,7 @@ class UsersController < ApplicationController
       @clickables.collect { |u| u.id }
     end
   end
-  
+
   def load_current_user
     if !params[:user].blank? && !params[:user][:id].blank?
       session[:current_user] = params[:user][:id]
@@ -215,23 +207,33 @@ class UsersController < ApplicationController
       session[:current_user] = params["selected_user" + @user.id.to_s].to_i
     end
   end
-  
+
+  def load_clicked_user
+    if !params[:clicked_user].blank?
+      User.find(params[:clicked_user])
+    end
+  end
+
   def process_click
-    case params[:commit]
-    when 'Next User'
-      User.transaction do
-        @clicked_user.lose_credit
-        @selected_user.gain_credit
+    if @clicked_user != @selected_user
+      case params[:commit]
+      when 'Next User'
+        User.transaction do
+          @clicked_user.lose_credit
+          @selected_user.gain_credit
+        end
+        flash.now[:notice] = 'Successfully clicked ' + @clicked_user.name + '.'
+      when 'End Clicking'
+        User.transaction do
+          @clicked_user.lose_credit
+          @selected_user.gain_credit
+        end
+        flash[:notice] = 'Successfully clicked ' + @clicked_user.name + '.'
+      when 'Skip User'
+        flash.now[:error] = 'Skipped ' + @clicked_user.name + '.'
       end
-      flash.now[:notice] = 'Successfully clicked ' + @clicked_user.name + '.'
-    when 'End Clicking'
-      User.transaction do
-        @clicked_user.lose_credit
-        @selected_user.gain_credit
-      end
+    else
       flash[:notice] = 'Successfully clicked ' + @clicked_user.name + '.'
-    when 'Skip User'
-      flash.now[:error] = 'Skipped ' + @clicked_user.name + '.'
     end
   end
 end
